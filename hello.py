28925d398b28870087fc078057739d345b2012a3
Emre Yesilkaya <ey@Emre-MacBook-Air.local>
2024-09-19 16:04:04 +0300

    python komutlarını ekledim

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
-print('hello world')
-print('nasılsın')
-#merhabalar
+import time
+from datetime import datetime
+
+while True:
+    # Şu anki zamanı al
+    current_time = datetime.now().strftime("%H:%M:%S")
+    print(f"Şu anki saat: {current_time}")
+    
+    # 60 saniye bekle
+    time.sleep(60)
